2332499a944827166e6ef6345db9a7b2dcd97928
student <2132>
2021-12-14 13:32:20 +0300

    start new arch

--- Models/authModel.py
+++
@@ -1,4 +0,0 @@
-from Models.ConnectDataBase import cur
-
-def AuthorizationUser(login, password):
-    return cur.execute(f"SELECT idResident FROM user WHERE login = '{login}' AND password = '{password}'").fetchone()
